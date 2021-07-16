ef4f9bf36af77b2a7536804b0796beb2b3017e6e
jvthomas90 <jvthomas90@gmail.com>
2021-07-16 00:48:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TipCalculator from './tipCalc';
+
+function App() {
+  return (
+    <div className="App">
+      <TipCalculator />
+    </div>
+  );
+}
+
+export default App;
